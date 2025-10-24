900c1118b4ab9ac0fefe908ac8bd7b659962870c
mario.martinezgarcia <mario.martinezgarcia@A208P24.informatica.cifpaviles.pa>
2025-10-24 12:23:31 +0200

    Rama C hecha por Mario.

---
+++ eclipse-workspace/RamaC/src/Mario/Mario.java
@@ -0,0 +1,14 @@
+package Mario;
+
+public class Mario {
+	public Mario() {
+		System.out.println("Mario Martínez García.");
+
+		
+	}
+
+	public static void main(String[] args) {
+		Mario unMario=new Mario();
+	}
+
+}
